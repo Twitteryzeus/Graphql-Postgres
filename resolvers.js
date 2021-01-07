a15f1850df1a4df8e86922a055b657c471a515c9
Brijesh Lakkad <brijesh@logicwind.co>
2021-01-07 15:36:10 +0530

    create all resolved functions

--- resolvers.js
+++ resolvers.js
@@ -1,16 +1,58 @@
 const bcrypt = require("bcryptjs");
+const events = require("./server/models/events");
 
 const resolvers = {
   Query: {
-    async user(root, { id }, context, info) {
+    async user(root, { id }, { models }, info) {
       try {
-        const user = await context.models.User.findByPk(id);
+        const user = await models.User.findByPk(id);
 
         if (!user) {
           throw new Error("No Such User");
         }
 
         return user;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+    async events(root, { id }, { models }, info) {
+      try {
+        const events = await models.Events.findByPk(id);
+
+        if (!events) {
+          throw new Error("No Such Error");
+        }
+
+        return events;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+    async allusers(root, args, { models }, info) {
+      try {
+        const users = await models.User.findAll({
+          include: models.Events,
+        });
+
+        if (!users) {
+          throw new Error("No users found");
+        }
+
+        return users;
+      } catch (error) {
+        console.log("Error", error);
+      }
+    },
+    async allevents(root, args, { models }, info) {
+      try {
+        const events = await models.Events.findAll();
+
+        if (!events) {
+          throw new Error("No Events Found");
+        }
+
+        return events;
       } catch (error) {
         console.log("Error", error);
       }
